f29bcf53cefcf595be9be12015812c9ab252dfdd
hellory4n <hellory4n@gmail.com>
2023-08-13 20:37:59 -0400

    windows™

--- Scripts/OS/Lelkstop/Lelsktop.cs
+++ Scripts/OS/Lelkstop/Lelsktop.cs
@@ -6,5 +6,11 @@ public class Lelsktop : Control {
         base._Ready();
         Vector2 bruh = ResolutionManager.GetScreenSize();
         GD.Print($"Screen resolution is {bruh.x}, {bruh.y}");
+
+        PackedScene m = ResourceLoader.Load<PackedScene>("res://Apps/WindowManagerTest/WindowManagerTest.tscn");
+        WindowDialog jjkn = (WindowDialog)m.Instance();
+        AddChild(jjkn);
+        jjkn.Popup_();
+        jjkn.PopupCentered(jjkn.RectSize);
     }
 }
