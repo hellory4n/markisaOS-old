d888c196c1e0304442b56d47f77cababb3798df4
hellory4n <hellory4n@gmail.com>
2023-08-14 19:45:08 -0400

    bruh

--- Scripts/OS/Lelkstop/Lelsktop.cs
+++ Scripts/OS/Lelkstop/Lelsktop.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public class Lelsktop : Control {
+public class Lelsktop : Node2D {
     public override void _Ready() {
         base._Ready();
         Vector2 bruh = ResolutionManager.GetScreenSize();
--- Scripts/OS/WindowManager/WindowDecoration.cs
+++ Scripts/OS/WindowManager/WindowDecoration.cs
@@ -6,22 +6,16 @@ public class WindowDecoration : KinematicBody2D {
     Vector2 GrabbedOffset = new Vector2();
 
     public override void _InputEvent(Godot.Object viewport, InputEvent @event, int shapeIdx) {
-        GD.Print("1");
         if (@event is InputEventMouseButton) {
-            GD.Print("2");
             CanDrag = @event.IsPressed();
             GrabbedOffset = Position - GetGlobalMousePosition();
         }
-        GD.Print("3");
         base._InputEvent(viewport, @event, shapeIdx);
     }
 
     public override void _Process(float delta) {
-        if (Input.IsMouseButtonPressed((int)ButtonList.Left) && CanDrag) {
+        if (Input.IsMouseButtonPressed((int)ButtonList.Left) && CanDrag) 
             Position = GetGlobalMousePosition() + GrabbedOffset;
-            GD.Print("4");
-        }
-
         base._Process(delta);
     }
 }
