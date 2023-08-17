ac438f15b46f7bc3a67097c294e8a96224ef9c14
hellory4n <hellory4n@gmail.com>
2023-08-17 19:05:53 -0400

    yes

--- Scripts/Apps/WindowManagerTest/EpicCoolAmazingBeautifulGorgeousFantasticMajesticButton.cs
+++ Scripts/Apps/WindowManagerTest/EpicCoolAmazingBeautifulGorgeousFantasticMajesticButton.cs
@@ -1,12 +1,13 @@
 using Godot;
 using System;
 
-public class Lelsktop : Node2D {
+public class EpicCoolAmazingBeautifulGorgeousFantasticMajesticButton : Button {
     public override void _Ready() {
         base._Ready();
-        Vector2 bruh = ResolutionManager.GetScreenSize();
-        GD.Print($"Screen resolution is {bruh.x}, {bruh.y}");
-        
+        Connect("pressed", this, nameof(Click));
+    }
+
+    public void Click() {
         WindowManager wm = GetNode<WindowManager>("/root/WindowManager");
         PackedScene m = ResourceLoader.Load<PackedScene>("res://Apps/WindowManagerTest/WindowManagerTest.tscn");
         WindowDialog jjkn = (WindowDialog)m.Instance();    
