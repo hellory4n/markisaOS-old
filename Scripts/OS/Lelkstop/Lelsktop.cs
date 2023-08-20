1082d6879470325ddf9594db9a2cf716dc2bf0f5
hellory4n <hellory4n@gmail.com>
2023-08-20 18:21:56 -0400

    cool animation or something idk

--- Scripts/OS/Lelkstop/Lelsktop.cs
+++ Scripts/OS/Lelkstop/Lelsktop.cs
@@ -14,6 +14,15 @@ public class Lelsktop : Node2D {
 
         // cool dock :)
         GetNode<Panel>("Painful/Panel").RectSize = new Vector2(bruh.x, 80);
-        GetNode<Panel>("Painful/Panel").RectPosition = new Vector2(0, bruh.y-100);
+
+        // play the animation for the dock and make sure the position on the animation is correct :)
+        Animation animationomg = GetNode<AnimationPlayer>("AnimationPlayer").GetAnimation("Startup");
+        // this didn't work
+        // int track = animationomg.FindTrack("Painful/Panel"); 
+        int keyStart = animationomg.TrackFindKey(0, 0);
+        int keyEnd = animationomg.TrackFindKey(0, 0.5f);
+        animationomg.TrackSetKeyValue(0, keyStart, new Vector2(0, bruh.y));
+        animationomg.TrackSetKeyValue(0, keyEnd, new Vector2(0, bruh.y-100));
+        GetNode<AnimationPlayer>("AnimationPlayer").Play("Startup");
     }
 }
