11261a1487575e64495b1dc1657e1b8f51e6da0e
hellory4n <hellory4n@gmail.com>
2023-08-21 16:41:55 -0400

    cool ui

--- Scripts/OS/WindowManager/SetSizeToScreenResolution.cs
+++ Scripts/OS/WindowManager/SetSizeToScreenResolution.cs
@@ -2,8 +2,11 @@ using Godot;
 using System;
 
 public class SetSizeToScreenResolution : Control {
+    [Export]
+    Vector2 Offset = new Vector2(0, 0);
+
     public override void _Ready() {
         base._Ready();
-        RectSize = ResolutionManager.GetScreenSize();
+        RectSize = ResolutionManager.GetScreenSize() - Offset;
     }
 }
