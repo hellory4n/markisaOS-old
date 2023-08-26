58fbaf43d821bfbf797f1967d941ab0b74f396b6
hellory4n <hellory4n@gmail.com>
2023-08-26 16:02:03 -0400

    thing that doesn't work :)

--- Scripts/OS/WindowManager/WindowManager.cs
+++ Scripts/OS/WindowManager/WindowManager.cs
@@ -2,12 +2,32 @@ using Godot;
 using System;
 using System.Collections.Generic;
 
-public class WindowManager : Node {
-    PackedScene openWindow;
+public class WindowManager : Node2D {
+    PackedScene OpenWindow;
+    Panel TextPeek;
+    public static string TextPeekText = "";
+    public static bool TextPeekVisible = false;
 
     public override void _Ready() {
         base._Ready();
-        openWindow = ResourceLoader.Load<PackedScene>("res://OS/Lelsktop/OpenWindowButton.tscn");
+        OpenWindow = ResourceLoader.Load<PackedScene>("res://OS/Lelsktop/OpenWindowButton.tscn");
+
+        // textpeek :omg:
+        PackedScene pain = ResourceLoader.Load<PackedScene>("res://OS/Lelsktop/Overlay/TextPeek.tscn");
+        TextPeek = (Panel)pain.Instance();
+        AddChild(TextPeek);
+        TextPeek.RectMinSize = new Vector2(ResolutionManager.GetScreenSize().x, 40);
+        TextPeek.RectPosition = new Vector2(69, 69);
+    }
+
+    public override void _Process(float delta) {
+        base._Process(delta);
+        if (TextPeekVisible) {
+            TextPeek.Visible = true;
+            TextPeek.GetNode<LineEdit>("Text").Text = TextPeekText;
+        } else {
+            TextPeek.Visible = false;
+        }
     }
 
     public void AddWindow(BaseWindow window) {
@@ -21,7 +41,7 @@ public class WindowManager : Node {
         window.RectPosition = yes/2 - (window.RectSize/2);
 
         // add it to the dock
-        OpenWindowButton coolDockButton = (OpenWindowButton)openWindow.Instance();
+        OpenWindowButton coolDockButton = (OpenWindowButton)OpenWindow.Instance();
         coolDockButton.Init(window);
         HBoxContainer dock = GetNode<HBoxContainer>("/root/LelsktopInterface/Dock/ScrollContainer/HBoxContainer");
         dock.AddChild(coolDockButton);
