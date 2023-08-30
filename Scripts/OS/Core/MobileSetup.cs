0129df4d25ce0d9b0d59e99ba70941f4be88b188
hellory4n <hellory4n@gmail.com>
2023-08-29 21:38:20 -0400

    mobile setup™™™ was broken lol

--- Scripts/OS/Core/MobileSetup.cs
+++ Scripts/OS/Core/MobileSetup.cs
@@ -2,7 +2,8 @@ using Godot;
 using System;
 
 public class MobileSetup : Control {
-    public int FailedAttempts = 0;
+    int FailedAttempts = 0;
+    int SuccessfulAttempts = 0;
 
     public override void _Ready() {
         base._Ready();
@@ -22,48 +23,50 @@ public class MobileSetup : Control {
         }
     }
 
-    // idk man
     public override void _GuiInput(InputEvent @event) {
-        if (Input.IsActionJustReleased("click_thingy")) {
-            FailedAttempts++;
+        if (@event is InputEventMouseButton fart) {
+            // if the button was successfully pressed it would be processed by the button first
+            if (fart.Pressed) {
+                // first update the resolution
+                DisplaySettings display = SavingManager.LoadSettings<DisplaySettings>(
+                    SavingManager.Settings.DisplaySettings
+                );
+                display.ScalingFactor += 0.2f;
+                SavingManager.SaveSettings(SavingManager.Settings.DisplaySettings, display);
+                ResolutionManager m = GetNode<ResolutionManager>("/root/ResolutionManager");
+                m.Update();
+
+                // then update the ui
+                RectSize = display.Resolution / display.ScalingFactor;
+                GetNode<Label>("Title").Text = "Let's try again";
+                GetNode<Label>("Text").Text = "We have updated the UI, try again to see if that works for you";
+                
+                // recalculate the scale for the background
+                float scale;
+                if (display.Resolution/display.ScalingFactor > new Vector2(1280, 720)) {
+                    scale = (Mathf.Max((display.Resolution/display.ScalingFactor).x,
+                        (display.Resolution/display.ScalingFactor).y) - 1280) / 1280;
+                    scale += 1;
+                } else {
+                    scale = Mathf.Max((display.Resolution/display.ScalingFactor).x,
+                        (display.Resolution/display.ScalingFactor).y) / 1280;
+                }
+                GetParent().GetNode<Sprite>("Background").Scale = new Vector2(scale, scale);
+                // idk why just diving the resolution by 2 doesn't work
+                GetParent().GetNode<Sprite>("Background").Position = new Vector2(
+                    display.Resolution.x/display.ScalingFactor, display.Resolution.y/display.ScalingFactor
+                ) / 2;
+
+                FailedAttempts = 0;
+                SuccessfulAttempts = 0;
+            }
         }
         base._GuiInput(@event);
     }
 
     public void Thing() {
-        if (FailedAttempts > 0) {
-            // first update the resolution
-            DisplaySettings display = SavingManager.LoadSettings<DisplaySettings>(
-                SavingManager.Settings.DisplaySettings
-            );
-            display.ScalingFactor += 0.2f;
-            SavingManager.SaveSettings(SavingManager.Settings.DisplaySettings, display);
-            ResolutionManager m = GetNode<ResolutionManager>("/root/ResolutionManager");
-            m.Update();
-
-            // then update the ui
-            RectSize = display.Resolution / display.ScalingFactor;
-            GetNode<Label>("Title").Text = "Let's try again";
-            GetNode<Label>("Text").Text = "We have updated the UI, try again to see if that works for you";
-            
-            // recalculate the scale for the background
-            float scale;
-            if (display.Resolution/display.ScalingFactor > new Vector2(1280, 720)) {
-                scale = (Mathf.Max((display.Resolution/display.ScalingFactor).x,
-                    (display.Resolution/display.ScalingFactor).y) - 1280) / 1280;
-                scale += 1;
-            } else {
-                scale = Mathf.Max((display.Resolution/display.ScalingFactor).x,
-                    (display.Resolution/display.ScalingFactor).y) / 1280;
-            }
-            GetParent().GetNode<Sprite>("Background").Scale = new Vector2(scale, scale);
-            // idk why just diving the resolution by 2 doesn't work
-            GetParent().GetNode<Sprite>("Background").Position = new Vector2(
-                display.Resolution.x/display.ScalingFactor, display.Resolution.y/display.ScalingFactor
-            ) / 2;
-
-            FailedAttempts = 0;
-        } else {
+        SuccessfulAttempts++;
+        if (SuccessfulAttempts == 3) {
             // this seems right, show the bootscreen
             DisplaySettings display = SavingManager.LoadSettings<DisplaySettings>(
                 SavingManager.Settings.DisplaySettings
