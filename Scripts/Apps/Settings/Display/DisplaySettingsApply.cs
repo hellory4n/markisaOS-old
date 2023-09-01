9bf37649bf5a1b333d51710b9df57bdad17812fd
hellory4n <hellory4n@gmail.com>
2023-09-01 14:54:32 -0400

    just gonna save this since i forgor to make the saving manager less terrible

---
+++ Scripts/Apps/Settings/Display/DisplaySettingsApply.cs
@@ -0,0 +1,11 @@
+using Godot;
+using System;
+
+public class DisplaySettingsApply : Button {
+    public override void _Ready() {
+        base._Ready();
+
+        // set the value of the spin boxes to the current settings
+        DisplaySettings display = SavingManager.LoadSettings<DisplaySettings>(SavingManager.Settings.DisplaySettings);
+    }
+}
