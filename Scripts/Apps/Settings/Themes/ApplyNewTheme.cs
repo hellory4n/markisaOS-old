4fd35c3542929f8e0576068b97002dc234d2a44b
hellory4n <hellory4n@gmail.com>
2023-09-02 21:59:32 -0400

    set theme omgomgogmogmgomgmggo

---
+++ Scripts/Apps/Settings/Themes/ApplyNewTheme.cs
@@ -0,0 +1,57 @@
+using Godot;
+using System;
+
+public class ApplyNewTheme : Button {
+    public override void _Ready() {
+        base._Ready();
+        Connect("pressed", this, nameof(Click));
+    }
+
+    public void Click() {
+        // a questionable way of getting the buttongroup :)))))))
+        ButtonGroup pain = GetNode<CheckBox>("../Grid/CheckBox").Group;
+
+        // load the theme :))))))
+        CheckBox option = (CheckBox)pain.GetPressedButton();
+        Theme theme;
+        switch (option.Text) {
+            case "Blue":
+                theme = ResourceLoader.Load<Theme>("res://Assets/Themes/Leltheme-Dark-Blue/Theme.tres");
+                break;
+            case "Green":
+                theme = ResourceLoader.Load<Theme>("res://Assets/Themes/Leltheme-Dark-Green/Theme.tres");
+                break;
+            case "Orange":
+                theme = ResourceLoader.Load<Theme>("res://Assets/Themes/Leltheme-Dark-Orange/Theme.tres");
+                break;
+            case "Pink":
+                theme = ResourceLoader.Load<Theme>("res://Assets/Themes/Leltheme-Dark-Pink/Theme.tres");
+                break;
+            case "Purple":
+                theme = ResourceLoader.Load<Theme>("res://Assets/Themes/Leltheme-Dark-Purple/Theme.tres");
+                break;
+            case "Red":
+                theme = ResourceLoader.Load<Theme>("res://Assets/Themes/Leltheme-Dark-Red/Theme.tres");
+                break;
+            case "Yellow":
+                theme = ResourceLoader.Load<Theme>("res://Assets/Themes/Leltheme-Dark-Yellow/Theme.tres");
+                break;
+            default:
+                theme = ResourceLoader.Load<Theme>("res://Assets/Themes/Leltheme-Dark-Blue/Theme.tres");
+                break;
+        }
+
+        // apply the theme :)))))))))))))))))))))))))))))
+        GetNode<Control>("/root/Lelsktop/Thing/Windows/ThemeThing").Theme = theme;
+        CanvasLayer lelsktopInterface = GetNode<CanvasLayer>("/root/LelsktopInterface");
+        lelsktopInterface.GetNode<Panel>("Dock").Theme = theme;
+        lelsktopInterface.GetNode<Panel>("QuickSettings").Theme = theme;
+        lelsktopInterface.GetNode<Panel>("AppMenu").Theme = theme;
+        lelsktopInterface.GetNode<Panel>("Panel").Theme = theme;
+
+        // save the settings :)))))))))))))))))))))))))))))))))))))))))))))))))
+        UserLelsktop asdadjffjsfjaf = SavingManager.Load<UserLelsktop>(SavingManager.CurrentUser);
+        asdadjffjsfjaf.Theme = $"Leltheme-Dark-{option.Text}";
+        SavingManager.Save<UserLelsktop>(SavingManager.CurrentUser, asdadjffjsfjaf);
+    }
+}
