cbdba3e89ae58a8cdfe7f82695d216faf0375292
hellory4n <hellory4n@gmail.com>
2023-09-11 15:40:32 -0400

    i think fixing a bug made a new bug and to fix the new bug i need to fix the old bug in a new way

--- OS/Scripts/Lelsktop/Lelsktop.cs
+++ OS/Scripts/Lelsktop/Lelsktop.cs
@@ -5,25 +5,6 @@ public class Lelsktop : Node2D {
     public override void _Ready() {
         base._Ready();
 
-        // testing epic formats :)
-        /*Picture suffering = new Picture(
-            "Lelcube omogmgogmom",
-            "res://icon.png",
-            LelfsManager.PermanentPath("/Malware"),
-            "me",
-            "ur mom's house",
-            "it's lelcube",
-            "h",
-            "if you use this i will literally fucking murder you"
-        );
-        suffering.Save();*/
-        Picture suffering = LelfsManager.Load<Picture>("/Malware/Lelcube omogmgogmom");
-        Sprite hahaFunni = new Sprite {
-            Centered = false,
-            Texture = suffering.GetResource()
-        };
-        AddChild(hahaFunni);
-
         Vector2 bruh = ResolutionManager.Resolution;
 
         Viewport pain = GetNode<Viewport>("/root/Lelsktop/Thing/Windows");
@@ -87,6 +68,7 @@ public class Lelsktop : Node2D {
             DefaultOpenWindowButton yes = packedScene.Instance<DefaultOpenWindowButton>();
             yes.Icon = ResourceLoader.Load<Texture>(app.Icon);
             yes.WindowScene = app.Scene;
+            yes.HintTooltip = app.Name;
             lelsktopInterface.GetNode<HBoxContainer>("Dock/DockStuff/QuickLaunch").AddChild(yes);
         }
     }
