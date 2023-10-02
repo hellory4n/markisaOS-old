bbecb96b363a010b263a6b907400659191a8587f
hellory4n <hellory4n@gmail.com>
2023-10-01 20:09:20 -0400

    it no worky :(

--- OS/Scripts/SavingManager/SavingManager.cs
+++ OS/Scripts/SavingManager/SavingManager.cs
@@ -271,14 +271,22 @@ public class SavingManager : Node {
         if (version.MajorVersion == 0 && version.MinorVersion == 7) {
             var coolApps = Load<InstalledApps>(user);
             // fun
-            coolApps.All = coolApps.All.Append(new Lelapp("Observer", "res://Apps/Observer/Assets/IconSmall.png", "res://Apps/Observer/Observer.tscn")).ToArray();
-            coolApps.Graphics = coolApps.Graphics.Append(new Lelapp("Observer", "res://Apps/Observer/Assets/IconSmall.png", "res://Apps/Observer/Observer.tscn")).ToArray();
-            coolApps.Multimedia = coolApps.Multimedia.Append(new Lelapp("Observer", "res://Apps/Observer/Assets/IconSmall.png", "res://Apps/Observer/Observer.tscn")).ToArray();
-            coolApps.Utilities = coolApps.Utilities.Append(new Lelapp("Observer", "res://Apps/Observer/Assets/IconSmall.png", "res://Apps/Observer/Observer.tscn")).ToArray();
-            coolApps.All = coolApps.All.Append(new Lelapp("Notebook", "res://Apps/Notebook/Assets/IconSmall.png", "res://Apps/Notebook/Notebook.tscn")).ToArray();
-            coolApps.Utilities = coolApps.Utilities.Append(new Lelapp("Notebook", "res://Apps/Notebook/Assets/IconSmall.png", "res://Apps/Notebook/Notebook.tscn")).ToArray();
-            coolApps.All = coolApps.All.Append(new Lelapp("Calculator", "res://Apps/Calculator/Assets/IconSmall.png", "res://Apps/Calculator/Calculator.tscn")).ToArray();
-            coolApps.Utilities = coolApps.Utilities.Append(new Lelapp("Calculator", "res://Apps/Calculator/Assets/IconSmall.png", "res://Apps/Calculator/Calculator.tscn")).ToArray();
+            var fuckAll = coolApps.All.ToList();
+            fuckAll.Add(new Lelapp("Observer", "res://Apps/Observer/Assets/IconSmall.png", "res://Apps/Observer/Observer.tscn"));
+            fuckAll.Add(new Lelapp("Notebook", "res://Apps/Notebook/Assets/IconSmall.png", "res://Apps/Notebook/Notebook.tscn"));
+            fuckAll.Add(new Lelapp("Calculator", "res://Apps/Calculator/Assets/IconSmall.png", "res://Apps/Calculator/Calculator.tscn"));
+            var fuckUtilities = coolApps.Utilities.ToList();
+            fuckUtilities.Add(new Lelapp("Observer", "res://Apps/Observer/Assets/IconSmall.png", "res://Apps/Observer/Observer.tscn"));
+            fuckUtilities.Add(new Lelapp("Notebook", "res://Apps/Notebook/Assets/IconSmall.png", "res://Apps/Notebook/Notebook.tscn"));
+            fuckUtilities.Add(new Lelapp("Calculator", "res://Apps/Calculator/Assets/IconSmall.png", "res://Apps/Calculator/Calculator.tscn"));
+            coolApps.All = fuckAll.ToArray();
+            coolApps.Utilities = fuckUtilities.ToArray();
+            var fuckGraphics = coolApps.Graphics.ToList();
+            fuckGraphics.Add(new Lelapp("Observer", "res://Apps/Observer/Assets/IconSmall.png", "res://Apps/Observer/Observer.tscn"));
+            coolApps.Graphics = fuckGraphics.ToArray();
+            var fuckMultimedia = coolApps.Multimedia.ToList();
+            fuckMultimedia.Add(new Lelapp("Observer", "res://Apps/Observer/Assets/IconSmall.png", "res://Apps/Observer/Observer.tscn"));
+            coolApps.Multimedia = fuckMultimedia.ToArray();
             Save(user, coolApps);
 
             version.MinorVersion = 8;
