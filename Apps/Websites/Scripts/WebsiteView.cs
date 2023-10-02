05e9649812e3c77f85714bee10396ddd04ce5ad3
hellory4n <hellory4n@gmail.com>
2023-10-02 19:44:56 -0400

    addresses + more websites omogomgogommgomog

---
+++ Apps/Websites/Scripts/WebsiteView.cs
@@ -0,0 +1,52 @@
+using Godot;
+using System;
+
+public class WebsiteView : Control {
+    string coolAddress = "web://passionfruit.com/lelcubeos/me/home.tscn";
+    Control previousThing;
+
+    public override void _Ready() {
+        base._Ready();
+        //GetNode<LineEdit>("Toolbar/Address").Connect("text_entered", this, nameof(LoadStuff));
+        LoadStuff(coolAddress);
+    }
+
+    public void LoadStuff(string newText) {
+        coolAddress = newText;
+        // parse the address :)
+        string h = coolAddress.Replace("web://", "res://Web/");
+        // maybe the user didn't put the "home.tscn" thing at the end
+        if (!ResourceLoader.Exists(h)) {
+            h += "/home.tscn";
+            GetNode<LineEdit>("Toolbar/Address").Text += "/home.tscn";
+        }
+        // or maybe the user forgor to put the web:// thingy
+        if (!ResourceLoader.Exists(h)) {
+            h = "res://Web/" + h;
+            GetNode<LineEdit>("Toolbar/Address").Text = "web://" + GetNode<LineEdit>("Toolbar/Address").Text;
+        }
+
+        // get the tab title
+        string tabTitle = h.Replace("res://Web/", "");
+        tabTitle = tabTitle.Substring(0, tabTitle.IndexOf('/'));
+
+        // actually load the website :)
+        if (!ResourceLoader.Exists(h)) {
+            previousThing?.QueueFree();
+            var ohShoes = ResourceLoader.Load<PackedScene>("res://Web/404/home.tscn");
+            var m = ohShoes.Instance<Control>();
+            m.MarginTop = 60;
+            AddChild(m);
+            previousThing = m;
+            GetNode<Label>("TabTitle").Text = "404";
+        } else {
+            previousThing?.QueueFree();
+            var jgkjfgkb = ResourceLoader.Load<PackedScene>(h);
+            var m = jgkjfgkb.Instance<Control>();
+            m.MarginTop = 60;
+            AddChild(m);
+            previousThing = m;
+            GetNode<Label>("TabTitle").Text = tabTitle;
+        }
+    }
+}
