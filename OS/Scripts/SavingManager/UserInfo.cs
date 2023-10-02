33f58d7ba862330f1229bc9f84ebffc5a55577c2
hellory4n <hellory4n@gmail.com>
2023-10-02 13:27:51 -0400

    0.9 :)

--- OS/Scripts/SavingManager/UserInfo.cs
+++ OS/Scripts/SavingManager/UserInfo.cs
@@ -9,7 +9,7 @@ public class BasicUser {
     /// <summary>
     /// Increased with smaller new versions, usually some new features and improvements.
     /// </summary>
-    public int MinorVersion = 8;
+    public int MinorVersion = 9;
     /// <summary>
     /// Increased with very small new versions, such as bug fixes.
     /// </summary>
