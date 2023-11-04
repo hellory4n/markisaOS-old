5a1bf28f16e3539b2b3619b1d2acaae3559442b8
hellory4n <hellory4n@gmail.com>
2023-11-04 18:56:16 -0400

    this does nothing, truly epic

---
+++ OS/Scripts/Lelsktop/Pinboard/Sticker.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+
+public class Sticker : KinematicBody2D {
+    bool CanDrag = false;
+    Vector2 GrabbedOffset = new Vector2();
+
+    public override void _InputEvent(Godot.Object viewport, InputEvent @event, int shapeIdx) {
+        if (@event is InputEventMouseButton) {
+            CanDrag = @event.IsPressed();
+            GrabbedOffset = Position - GetGlobalMousePosition();
+        }
+        base._InputEvent(viewport, @event, shapeIdx);
+    }
+
+    public override void _Process(float delta) {
+        if (Input.IsMouseButtonPressed((int)ButtonList.Left) && CanDrag) 
+            Position = GetGlobalMousePosition() + GrabbedOffset;
+        base._Process(delta);
+    }
+}
