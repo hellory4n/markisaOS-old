9bc30f5e9327961d771d0ce8b0ba4192510b98ff
hellory4n <hellory4n@gmail.com>
2023-11-06 09:47:58 -0400

    yes

--- OS/Scripts/Lelsktop/Pinboard/Sticker.cs
+++ OS/Scripts/Lelsktop/Pinboard/Sticker.cs
@@ -2,10 +2,11 @@ using Godot;
 using System;
 
 public class Sticker : Sprite {
-    // based on https://gist.github.com/angstyloop/08200c6d816347c82ea1aed56c219f17
+    // this is stolen from https://gist.github.com/angstyloop/08200c6d816347c82ea1aed56c219f17
     enum StatusThingy {
         None,
         Clicked,
+        Released,
         Dragging
     }
 
@@ -14,7 +15,6 @@ public class Sticker : Sprite {
     public string PinboardItem;
     Vector2 EpicOffset;
     public static Sticker SelectedSticker;
-    bool MousePressed = false;
 
     public override void _Process(float delta) {
         base._Process(delta);
@@ -32,7 +32,7 @@ public class Sticker : Sprite {
 
         if (@event is InputEventMouseButton yes) {
             if (yes.ButtonIndex == (int)ButtonList.Left) {
-                if (yes.Pressed) {
+                if (Status != StatusThingy.Dragging && yes.Pressed) {
                     Rect2 aRect = new Rect2(
                         Position.x - Texture.GetSize().x * Scale.x / 2, Position.y - Texture.GetSize().y * Scale.y / 2,
                         Texture.GetSize().x * Scale.x, Texture.GetSize().y * Scale.y
@@ -44,32 +44,21 @@ public class Sticker : Sprite {
                         EpicOffset = Position - yes.Position;
                         SelectedSticker = this;
                     }
-                } else if (Status == StatusThingy.Clicked && SelectedSticker == this) {
-                    MousePressed = true;
-                    Status = StatusThingy.Dragging;
+                } else if (Status == StatusThingy.Dragging && !yes.Pressed) {
+                    Status = StatusThingy.Released;
 
-                    
+                    // we need to save the position :)))
+                    var pinboard = SavingManager.Load<LelsktopPinboard>(SavingManager.CurrentUser);
+                    pinboard.Items[PinboardItem].Position = Position;
+                    SavingManager.Save(SavingManager.CurrentUser, pinboard);
+
+                    SelectedSticker = null;
                 }
             }
         }
 
-        if (Status == StatusThingy.Dragging && MousePressed && @event is InputEventMouseMotion 
-        && SelectedSticker == this) {
-                Position = MousePosition + EpicOffset;
-        }
-
-
-        if (@event is InputEventMouseButton j) {
-            if (j.ButtonIndex == (int)ButtonList.Left && !j.Pressed) {
-                MousePressed = false;
-                SelectedSticker = null;
-                Status = StatusThingy.None;
-                
-                // we need to save the position :)))
-                var pinboard = SavingManager.Load<LelsktopPinboard>(SavingManager.CurrentUser);
-                pinboard.Items[PinboardItem].Position = Position;
-                SavingManager.Save(SavingManager.CurrentUser, pinboard);
-            }
+        if (Status == StatusThingy.Clicked && SelectedSticker == this && @event is InputEventMouseMotion) {
+            Status = StatusThingy.Dragging;
         }
     }
 }
