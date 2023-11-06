1be5a78c05f9df7a0c2c8205d860393793bdf363
hellory4n <hellory4n@gmail.com>
2023-11-06 12:05:28 -0400

    working 100% correctly and not broken at all

--- OS/Scripts/Lelsktop/Pinboard/PinboardSelectThingy.cs
+++ OS/Scripts/Lelsktop/Pinboard/PinboardSelectThingy.cs
@@ -3,16 +3,21 @@ using System;
 
 public class PinboardSelectThingy : Node2D {
     Control Yes;
+    public static Rect2 IncreaseSize;
+    public static Rect2 DecreaseSize;
+    public static Rect2 RemoveSticker;
 
     public override void _Ready() {
         base._Ready();
         Yes = GetNode<Control>("Yes");
+        Yes.RectSize = ResolutionManager.Resolution;
+        IncreaseSize = Yes.GetNode<Panel>("IncreaseSize").GetRect();
+        DecreaseSize = Yes.GetNode<Panel>("DecreaseSize").GetRect();
+        RemoveSticker = Yes.GetNode<Panel>("Delete").GetRect();
     }
 
     public override void _Process(float delta) {
         base._Process(delta);
-        Yes.RectSize = ResolutionManager.Resolution;
-        
         // themes are certainly cool & stuff
         Yes.Theme = GetNode<Control>("../1/Windows/ThemeThing").Theme;
 
--- OS/Scripts/Lelsktop/Pinboard/Sticker.cs
+++ OS/Scripts/Lelsktop/Pinboard/Sticker.cs
@@ -15,6 +15,14 @@ public class Sticker : Sprite {
     public string PinboardItem;
     Vector2 EpicOffset;
     public static Sticker SelectedSticker;
+    Timer Smaller;
+    Timer Bigger;
+
+    public override void _Ready() {
+        base._Ready();
+        Smaller = GetNode<Timer>("Smaller");
+        Bigger = GetNode<Timer>("Bigger");
+    }
 
     public override void _Process(float delta) {
         base._Process(delta);
@@ -43,6 +51,19 @@ public class Sticker : Sprite {
                         Status = StatusThingy.Clicked;
                         EpicOffset = Position - yes.Position;
                         SelectedSticker = this;
+
+                        // change size :)))))
+                        if (PinboardSelectThingy.DecreaseSize.Intersects(aRect)) {
+                            Smaller.Start();
+                        } else {
+                            Smaller.Stop();
+                        }
+
+                        if (PinboardSelectThingy.IncreaseSize.Intersects(aRect)) {
+                            Bigger.Start();
+                        } else {
+                            Bigger.Stop();
+                        }
                     }
                 } else if (Status == StatusThingy.Dragging && !yes.Pressed) {
                     Status = StatusThingy.Released;
@@ -60,5 +81,27 @@ public class Sticker : Sprite {
         if (Status == StatusThingy.Clicked && SelectedSticker == this && @event is InputEventMouseMotion) {
             Status = StatusThingy.Dragging;
         }
+    }
+
+    public void GetSmallerOmgomgomg() {
+        float nfjggjfg = (float)Math.Log(Scale.x + 0.1, 10);
+        float help = (float)Math.Pow(10, nfjggjfg - 0.2);
+
+        Scale = new Vector2(help, help);
+
+        var pinboard = SavingManager.Load<LelsktopPinboard>(SavingManager.CurrentUser);
+        pinboard.Items[PinboardItem].Scale = help;
+        SavingManager.Save(SavingManager.CurrentUser, pinboard);
+    }
+
+    public void GetBiggerOmgomgomg() {
+        float nfjggjfg = (float)Math.Log(Scale.x + 0.1, 10);
+        float help = (float)Math.Pow(10, nfjggjfg + 0.2);
+
+        Scale = new Vector2(help, help);
+
+        var pinboard = SavingManager.Load<LelsktopPinboard>(SavingManager.CurrentUser);
+        pinboard.Items[PinboardItem].Scale = help;
+        SavingManager.Save(SavingManager.CurrentUser, pinboard);
     }
 }
