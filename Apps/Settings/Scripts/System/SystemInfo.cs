59fa038fbd2a86daa0b67ad0047870c60bd31383
hellory4n <hellory4n@gmail.com>
2023-11-08 22:41:42 -0400

    stupidity

---
+++ Apps/Settings/Scripts/System/SystemInfo.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+using System.Reflection;
+
+public class SystemInfo : Label {
+    public override void _Ready() {
+        base._Ready();
+        BasicUser version = SavingManager.Load<BasicUser>(SavingManager.CurrentUser);
+        string model;
+        if (OS.GetModelName() == "GenericDevice")
+            model = "Unable to get model name™";
+        else
+            model = OS.GetModelName();
+
+        string cpu;
+        if (OS.GetProcessorName() == "")
+            cpu = "Unable to get CPU name™";
+        else
+            cpu = OS.GetProcessorName();
+
+        Text = $"lelcubeOS v{version.MajorVersion}.{version.MinorVersion}.{version.PatchVersion}\n© Passionfruit 2069\nModel {model}\n{cpu}";
+    }
+}
